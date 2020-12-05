0f73f713647f6cf56308f1c35017a047aa75b850
ArtMichel <a2019116758@estudiantes.upsa.edu.bo>
2020-12-05 11:30:00 -0400

    Se creo el modelo

---
+++ ExamenFinal1/ExamenFinal1/Controllers/OperacionController.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace ExamenFinal1.Controllers
+{
+    public class OperacionController : ApiController
+    {
+    }
+}
---
+++ ExamenFinal1/ExamenFinal1/Models/operación.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace ExamenFinal1.Models
+{
+    public class operación
+    {
+        [Key]
+        public int numero { get; set; }
+        public string Operacion
+        {
+            get
+            {
+                if (numero < 0)
+                {
+                    return "ERROR";
+                }
+                else if (numero == 0)
+                {
+                    return "Arturo Michel";
+                }
+                else 
+                { 
+                    return $"{numero}"; 
+                }
+            }
+        }
+    }
+}
